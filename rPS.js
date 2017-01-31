f24ebb4913144b74cf0db9df166a8d89dc033eb5
Eugene Kim <eugene.edward.kim@gmail.com>
2017-01-30 23:21:43 -0700

    Rock Paper Scissors update

--- rPS.js
+++ rPS.js
@@ -2,12 +2,16 @@ $('#rock').click(function(){
     $('#computer').empty()
     var computer = Math.floor(Math.random() * 3 + 1)
     var user = 1
+    var scoreComp = 0
+    var scoreYou = 0
     if(computer === 2) {
         $('#computer').text('Paper, you lose!');
+        $('#scoreComp').value(parseInt(this.innerHTML) + 1);
     } else if(computer = user) {
         $('#computer').text('Rock, we tie!');
     } else {
-        $('#computer').text('Scissors, you win you lucky duck.')
+        $('#computer').text('Scissors, you win you lucky duck.');
+        $('#scoreUser').value(parseInt(this.innerHTML) + 1);
     }
 })
 
@@ -17,10 +21,12 @@ $('#paper').click(function(){
     var user = 2
     if(computer === 3) {
         $('#computer').text('Scissors, you lose so hard!');
+        $('#scoreComp').value(parseInt(this.innerHTML) + 1);
     } else if(computer === 1) {
         $('#computer').text('Rock, you won, but that means nothing!');
+        $('#scoreUser').value(parseInt(this.innerHTML) + 1);
     } else {
-        $('#computer').text('Paper, we tie.')
+        $('#computer').text('Paper, we tie.');
     }
 })
 
@@ -30,9 +36,11 @@ $('#scissors').click(function(){
     var user = 3
     if(computer === 2) {
         $('#computer').text('Paper, I guess you won!');
+        $('#scoreUser').value(parseInt(this.innerHTML) + 1);
     } else if(computer === 1) {
         $('#computer').text('Rock, you lose teehee!');
+        $('#scoreComp').value(parseInt(this.innerHTML) + 1);
     } else {
-        $('#computer').text('Scissors, looks like we tie again.')
+        $('#computer').text('Scissors, looks like we tie again.');
     }
 })
